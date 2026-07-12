477b0cd741323d9b7a7a2d25a397475c6448e4a2
Daniel Berlin <dberlin@dberlin.org>
2026-07-12 17:28:16 -0400

    Fix display test

--- tests/test_display_launch.py
+++ tests/test_display_launch.py
@@ -27,7 +27,7 @@ def test_bare_for_pygame_display():
 def test_cage_for_qtquick_flex():
 	settings = {'modules': {'display': 'qtquick_flex'}}
 	argv, env_updates = display_launch.build_launch_argv(settings, {'XDG_RUNTIME_DIR': '/run/user/1000'})
-	assert argv == ['cage', '-s', '--', sys.executable, 'display_process.py']
+	assert argv == ['cage', '-d', '-s', '--', sys.executable, 'display_process.py']
 	assert env_updates['QT_QPA_PLATFORM'] == 'wayland'
 	assert env_updates['XDG_RUNTIME_DIR'] == '/run/user/1000'
 
@@ -35,7 +35,7 @@ def test_cage_for_qtquick_flex():
 def test_cage_for_qtquick_dsi():
 	settings = {'modules': {'display': 'qtquick_dsi_1280x720t'}}
 	argv, _env = display_launch.build_launch_argv(settings, {})
-	assert argv == ['cage', '-s', '--', sys.executable, 'display_process.py']
+	assert argv == ['cage', '-d', '-s', '--', sys.executable, 'display_process.py']
 
 
 def test_xdg_runtime_dir_preserved_when_set():
